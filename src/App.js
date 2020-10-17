44775ef2e3075c8ddf9dfd182bf44a6c00ca7067
Mahesh Khatiwada <maheshkhatiwada17@gmail.com>
2020-10-17 21:18:42 +0545

    adding and removing todo lists

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
+import Form from "./components/form";
+import TodoList from './components/todolist';
+
 
 function App() {
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+  
   return (
-    <h1>Hello WOrld</h1>  );
+    <div className="container">
+      <h1>Mahesh's Todo List</h1>
+      <br />
+      <h2>You have {todos.length} todo lists!!!</h2>
+      
+      
+      <Form
+        input={input}
+        todos={todos}
+        setInput={setInput}
+        setTodos={setTodos}
+      />
+
+      <TodoList
+        todos={todos}
+        setTodos={setTodos}
+      
+      />
+    </div>
+    
+  );
 }
 
 export default App;
